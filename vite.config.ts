2a039e4864703ff4a3d1321169fe1b21f73720d5
Braymen <mail@pessman.com>
2026-09-03 13:04:57 -0500

    feat: github pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    // Served from https://braymen.github.io/project-world/
-    base: '/project-world/',
+    base: '/environmental/',
     plugins: [react()],
 })
